4b07ad27011272f7ab447718ffd3f01684645990
Hayato13 <50024152+Hayato13@users.noreply.github.com>
2019-05-14 21:19:33 -0700

    Update engine.js
    -Changed star to stopwatch

--- js/engine.js
+++ js/engine.js
@@ -184,7 +184,7 @@ var Engine = (function(global) {
         'images/char-horn-girl.png',
         'images/char-pink-girl.png',
         'images/char-princess-girl.png',
-        'images/Star.png'
+        'images/stopwatch.png'
     ]);
     Resources.onReady(init);
 
